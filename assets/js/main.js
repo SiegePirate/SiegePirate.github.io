2dfc9221695dd6fecfb07cfb909b09ab5b3f4808
SiegePirate <101225501+SiegePirate@users.noreply.github.com>
2026-02-20 03:00:25 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const fadeElements = document.querySelectorAll(".fade-in");
 
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+
+  fadeElements.forEach(el => observer.observe(el));
+});
