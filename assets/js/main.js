192d08e1eaeb0bda1f2609585ef398c0d7c13b58
SiegePirate <101225501+SiegePirate@users.noreply.github.com>
2026-02-20 15:20:22 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const fadeElements = document.querySelectorAll(".fade-in");
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  });
-
-  fadeElements.forEach(el => observer.observe(el));
+  console.log("The United Seas is live.");
 });
