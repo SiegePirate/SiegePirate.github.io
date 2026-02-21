9e1e141601855c9e528e16679a4e494fd99a890a
SiegePirate <101225501+SiegePirate@users.noreply.github.com>
2026-02-20 22:51:34 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.getElementById("year");
+  const currentYear = new Date().getFullYear();
+
+  if (yearSpan) {
+    yearSpan.textContent = currentYear;
+  }
+
   console.log("The United Seas sails strong.");
 });
